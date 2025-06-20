a9327f9b1b9ca9d5918ef78aafd648d818d88494
fiqriBTI <fikri.dev@btigroup.ee>
2025-06-21 00:33:29 +0700

    feat: lps ui done

--- src/features/liquidityPool/components/CreatePoolForm.tsx
+++ src/features/liquidityPool/components/CreatePoolForm.tsx
@@ -5,6 +5,7 @@ import { ChevronDown } from 'lucide-react'
 import Image from 'next/image'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import { FaPlus } from 'react-icons/fa6'
 import { PiPencilLight } from 'react-icons/pi'
 
 import { Button } from '@/components/ui/button'
@@ -14,6 +15,7 @@ import { Input } from '@/components/ui/input'
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select'
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs'
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
+import SwapItem from '@/features/swap/components/SwapItem'
 import TokenListDialog from '@/features/swap/components/TokenListDialog'
 import { TTokenProps } from '@/features/swap/types'
 import FormProgressLine from '@/features/tokens/components/form/FormProgressLine'
@@ -128,6 +130,8 @@ export default function CreatePoolForm() {
 
 	const onSelectBaseToken = (value: TTokenProps) => form.setValue('baseToken', value, { shouldValidate: true })
 	const onSelectQuoteToken = (value: TTokenProps) => form.setValue('quoteToken', value, { shouldValidate: true })
+	const onInputBaseTokenAmount = (value: string) => form.setValue('baseTokenAmount', value, { shouldValidate: true })
+	const onInputQuoteTokenAmount = (value: string) => form.setValue('quoteTokenAmount', value, { shouldValidate: true })
 	const onSelectRangeType = (value: string) => {
 		form.setValue('rangeType', value, { shouldValidate: true })
 		if (value === 'custom-range') {
@@ -141,7 +145,7 @@ export default function CreatePoolForm() {
 
 	return (
 		<Form {...form}>
-			<form className="flex flex-col justify-center items-center space-y-6">
+			<form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-center items-center space-y-6">
 				<div className="md:w-[763px] w-full">
 					<FormProgressLine currentStep={currentStep} steps={createPoolSteps} />
 				</div>
@@ -372,7 +376,7 @@ export default function CreatePoolForm() {
 					</Card>
 				)}
 				{currentStep === 2 && (
-					<div>
+					<div className="flex flex-col space-y-6">
 						<Card className="md:w-[500px] w-full border-hover-green border-[1px] rounded-[12px] md:p-6 p-3 drop-shadow-lg">
 							<CardContent className="p-0 flex flex-col space-y-[18px]">
 								<div className="flex items-center justify-between">
@@ -410,122 +414,62 @@ export default function CreatePoolForm() {
 						</Card>
 						<Card className="md:w-[500px] w-full border-hover-green border-[1px] rounded-[12px] md:p-6 p-3 drop-shadow-lg">
 							<CardContent className="p-0 flex flex-col space-y-[18px]">
-								<section className="flex justify-between items-center">
-									<h3 className="text-base font-medium text-main-black">Price Settings</h3>
-									<ToggleGroup
-										type="single"
-										value={form.watch('priceSetting')}
-										onValueChange={(val) => {
-											if (val) form.setValue('priceSetting', val)
-										}}
-										className="bg-light-green gap-0 rounded-[4px] text-light-grey"
-									>
-										<ToggleGroupItem
-											value="wbnb"
-											className={cn(
-												'text-sm px-2.5 py-1 rounded-[4px] hover:bg-main-green hover:text-white',
-												form.getValues('priceSetting') === 'wbnb' && '!bg-main-green  !text-white '
-											)}
+								<section className="relative">
+									<div className="flex flex-col space-y-3">
+										<SwapItem
+											noTitle
+											type="from"
+											tokenProps={form.getValues('baseToken')}
+											inputAmount={form.getValues('baseTokenAmount')}
+											setInputAmount={onInputBaseTokenAmount}
+										/>
+										<SwapItem
+											noTitle
+											type="to"
+											tokenProps={form.getValues('quoteToken')}
+											inputAmount={form.getValues('quoteTokenAmount')}
+											setInputAmount={onInputQuoteTokenAmount}
+										/>
+									</div>
+									<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+										<Button
+											type="button"
+											variant="ghost"
+											size="icon"
+											className="rounded-full [&_svg]:size-8 bg-main-green text-white w-14 h-14"
 										>
-											WBNB Price
-										</ToggleGroupItem>
-										<ToggleGroupItem
-											value="usdt"
-											className={cn(
-												'text-sm px-2.5 py-1 rounded-[4px] hover:bg-main-green hover:text-white',
-												form.getValues('priceSetting') === 'usdt' && '!bg-main-green  !text-white'
-											)}
-										>
-											USDT Price
-										</ToggleGroupItem>
-									</ToggleGroup>
+											<FaPlus width={24} height={24} />
+										</Button>
+									</div>
 								</section>
-								<FormField
-									control={form.control}
-									name="initialPrice"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel className="text-main-black font-normal text-base">Initial Price</FormLabel>
-											<FormControl>
-												<Input
-													className="focus-visible:ring-hover-green focus:border-hover-green rounded-[8px] w-full"
-													type="text"
-													placeholder="Enter your initial price"
-													{...field}
-												/>
-											</FormControl>
-											<FormMessage className="text-sm" />
-										</FormItem>
-									)}
-								/>
-								<section className="flex flex-col space-y-2">
-									<h4 className="text-main-black font-normal text-base">Initial Price</h4>
-									<Tabs defaultValue="full-range" onValueChange={(value) => onSelectRangeType(value)}>
-										<TabsList className="bg-light-green p-0 w-full h-8 mb-[7px]">
-											<TabsTrigger
-												className="w-full h-full bg-transparent text-sm text-light-grey font-normal pt-1 hover:bg-main-green hover:text-main-white  focus-visible:bg-main-green focus-visible:text-main-white data-[state=active]:bg-main-green data-[state=active]:text-main-white data-[state=active]:rounded-[4px]"
-												value="full-range"
-											>
-												Full Range
-											</TabsTrigger>
-											<TabsTrigger
-												className="w-full h-full bg-transparent text-sm text-light-grey font-normal pt-1 hover:bg-main-green hover:text-main-white  focus-visible:bg-main-green focus-visible:text-main-white data-[state=active]:bg-main-green data-[state=active]:text-main-white data-[state=active]:rounded-[4px]"
-												value="custom-range"
-											>
-												Custom Range
-											</TabsTrigger>
-										</TabsList>
-										<TabsContent value="full-range"></TabsContent>
-										<TabsContent value="custom-range">
-											<div className="flex w-full p-2.5 space-x-2.5 justify-between items-center">
-												<FormField
-													control={form.control}
-													name="minInitialPrice"
-													render={({ field }) => (
-														<FormItem className="w-full">
-															<FormLabel className="text-base text-main-black font-normal">Min</FormLabel>
-															<FormControl className="w-full">
-																<Input
-																	className="focus-visible:ring-hover-green focus:border-hover-green rounded-[8px] w-full"
-																	type="text"
-																	placeholder="Enter your min initial price"
-																	{...field}
-																/>
-															</FormControl>
-															<FormMessage className="text-sm" />
-														</FormItem>
-													)}
-												/>
-												<FormField
-													control={form.control}
-													name="maxInitialPrice"
-													render={({ field }) => (
-														<FormItem className="w-full">
-															<FormLabel className="text-base text-main-black font-normal">Max</FormLabel>
-															<FormControl className="w-full">
-																<Input
-																	className="focus-visible:ring-hover-green focus:border-hover-green rounded-[8px] w-full"
-																	type="text"
-																	placeholder="Enter your max initial price"
-																	{...field}
-																/>
-															</FormControl>
-															<FormMessage className="text-sm" />
-														</FormItem>
-													)}
-												/>
-											</div>
-										</TabsContent>
-									</Tabs>
-								</section>
+								<div className="flex flex-col space-y-2.5 border-2 border-dark-grey rounded-[10px] p-2.5">
+									<section className="flex text-xs justify-between">
+										<p className="text-dark-grey">Total Deposit</p>
+										<p className="text-main-black">$0.00</p>
+									</section>
+									<section className="flex text-xs justify-between">
+										<p className="text-dark-grey">Deposit Ratio</p>
+										<div className="flex space-x-[5px] items-center">
+											<section className="flex space-x-[5px] items-center">
+												<Image src={form.getValues('baseToken').icon} width={13} height={13} alt="base token icon" />
+												<p className="text-xs text-main-black font-normal">0%</p>
+											</section>
+											<span className="text-xs text-main-black font-normal">/</span>
+											<section className="flex space-x-[5px] items-center">
+												<Image src={form.getValues('quoteToken').icon} width={13} height={13} alt="quote token icon" />
+												<p className="text-xs text-main-black font-normal">0%</p>
+											</section>
+										</div>
+									</section>
+								</div>
 							</CardContent>
 							<CardFooter className="pt-[18px] !px-0 !pb-0">
 								<Button
-									onClick={onNext}
-									type="button"
-									className="rounded-[48px] md:h-[55px] h-12 text-base md:text-xl py-3 w-full text-main-white bg-main-green hover:bg-hover-green"
+									disabled={!form.formState.isValid}
+									type="submit"
+									className="rounded-[48px] md:h-[55px]  h-12 text-base md:text-xl py-3 w-full text-main-white bg-main-green hover:bg-hover-green"
 								>
-									Continue
+									Submit
 								</Button>
 							</CardFooter>
 						</Card>
