d36eedc2dc55f51b795b9739d98b78c868369af5
fiqriBTI <fikri.dev@btigroup.ee>
2025-08-08 18:27:38 +0700

    feat: swap production ready

--- src/components/layout/WalletDialog.tsx
+++ src/components/layout/WalletDialog.tsx
@@ -12,6 +12,7 @@ import {
 	DialogDescription,
 	DialogClose
 } from '@/components/ui/dialog'
+import { isProduction } from '@/lib/utils'
 import { useWalletListDialog } from '@/stores/walletDialog'
 
 export default function WalletListDialog() {
@@ -30,20 +31,25 @@ export default function WalletListDialog() {
 				</DialogHeader>
 
 				<div className="grid grid-cols-3 md:mt-[22px] pb-3.5 border-b-2 border-b-light-grey justify-items-center md:gap-[42px] gap-4">
-					{wallets.map((wallet) => (
-						<DialogClose key={wallet.adapter.name} asChild>
-							<Button
-								onClick={() => {
-									select(wallet.adapter.name)
-								}}
-								variant="ghost"
-								className="flex flex-col items-center justify-end md:w-[105px] w-[54px] md:min-h-[85px] min-h-[76px] space-y-2"
-							>
-								<Image src={wallet.adapter.icon} width={42} height={42} alt={`${wallet.adapter.name} icon`} />
-								<h5 className="text-xs text-main-black text-center">{wallet.adapter.name}</h5>
-							</Button>
-						</DialogClose>
-					))}
+					{wallets
+						.filter((wallet) => {
+							if (isProduction) return wallet.adapter.name !== 'Burner Wallet'
+							return true
+						})
+						.map((wallet) => (
+							<DialogClose key={wallet.adapter.name} asChild>
+								<Button
+									onClick={() => {
+										select(wallet.adapter.name)
+									}}
+									variant="ghost"
+									className="flex flex-col items-center justify-end md:w-[105px] w-[54px] md:min-h-[85px] min-h-[76px] space-y-2"
+								>
+									<Image src={wallet.adapter.icon} width={42} height={42} alt={`${wallet.adapter.name} icon`} />
+									<h5 className="text-xs text-main-black text-center">{wallet.adapter.name}</h5>
+								</Button>
+							</DialogClose>
+						))}
 				</div>
 			</DialogContent>
 		</Dialog>
